8fc57f992165c6f9d77ac7f8f911459987635648
Hannu Jaakkola <hannu.jaakkola@w3.fi>
2016-04-22 23:14:47 +0300

    Changed texts

--- app/components/Help.js
+++ app/components/Help.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Help = props => (
   <div className="help">
-    <h3>Youtube channel autoplay</h3>
-    <p>You need to search with channel's exact name, sorry!</p>
+    <h3>Channel's video autoplay</h3>
+    <p>Automatically play a channel's recent uploads</p>
   </div>
 )
 
