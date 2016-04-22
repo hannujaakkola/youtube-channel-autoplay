3ffd3d3c7bac0ed6f2c286ab31d541ebad0575e7
Hannu Jaakkola <hannu.jaakkola@w3.fi>
2016-04-23 01:06:20 +0300

    Updated title

--- app/components/Help.js
+++ app/components/Help.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Help = props => (
   <div className="help">
-    <h3>Channel's video autoplay</h3>
-    <p>Automatically play a channel's recent uploads</p>
+    <h3>YouTube Channel Autoplay</h3>
+    <p>Automatically play a YouTube channel's recent uploads</p>
   </div>
 )
 
